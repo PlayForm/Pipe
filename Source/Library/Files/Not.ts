528d0a6d1bb8bcf98fa4feabe2d79bd5a96218f8
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-08 12:52:21 +0300

    squash!

--- Source/Library/Files/Not.ts
+++ Source/Library/Files/Not.ts
@@ -1,21 +1,43 @@
 import type { Options, Plan } from "../../Option/Index.js";
 
-export default async (Pattern: Options["Exclude"], Files: Plan["Results"]) =>
-	new Map(
-		Array.from(Files).filter(
-			([Key, Value]) =>
-				!(
-					Pattern instanceof Array
-						? Pattern
-						: Pattern instanceof Set
-						? Array.from(Pattern)
-						: [Pattern]
-				).some(
-					(Filter) =>
-						(typeof Filter === "string" &&
-							(Key.match(Filter) || Value.match(Filter))) ||
-						(typeof Filter === "function" &&
-							(Filter(Key) || Filter(Value)))
-				)
-		)
-	);
+export default async (Pattern: Options["Exclude"], Files: Plan["Results"]) => {
+	const Filters = new Set();
+
+	if (typeof Pattern !== "undefined") {
+		if (Pattern instanceof Array || Pattern instanceof Set) {
+			for (const Patterns of Pattern) {
+				Filters.add(Patterns);
+			}
+		} else {
+			Filters.add(Pattern);
+		}
+	}
+
+	for (const Filter of Filters) {
+		for (const File of Files) {
+			switch (true) {
+				case typeof Filter === "string": {
+					if (
+						File[0].match(Filter as string) ||
+						File[1].match(Filter as string)
+					) {
+						Files.delete(File[0]);
+					}
+
+					break;
+				}
+
+				case typeof Filter === "function": {
+					if (
+						(Filter as Function)(File[0]) ||
+						(Filter as Function)(File[1])
+					) {
+						Files.delete(File[0]);
+					}
+				}
+			}
+		}
+	}
+
+	return Files;
+};
