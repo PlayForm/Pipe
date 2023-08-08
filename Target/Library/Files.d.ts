f5fa1efd627c4d1da7bc7a3c8615d55cde9e659d
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-08 22:28:58 +0300

    squash!

--- Target/Library/Files.d.ts
+++ Target/Library/Files.d.ts
@@ -2,8 +2,8 @@ import type { Pattern } from "fast-glob";
 import type { Executions, Plan, Path, Options } from "../Option/Index.js";
 export default class Files {
     Pipe: (Callbacks?: Executions) => Promise<Plan>;
-    Not: (Pattern: Options["Exclude"]) => Promise<this>;
-    By: (Glob?: Pattern | Pattern[]) => Promise<this>;
+    Not: (File: Options["Exclude"]) => Promise<this>;
+    By: (File?: Pattern | Pattern[]) => Promise<this>;
     In: (Path?: Path) => Promise<this>;
     Plan: Plan;
     constructor(Debug?: Plan["Debug"]);
