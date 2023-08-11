a5946b0b8620b4bb09cf8dfa018e3223a5fbc81b
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-11 10:56:27 +0300

    squash!

--- Target/Library/Files.js
+++ Target/Library/Files.js
@@ -1,1 +1,7 @@
-import e from"../Option/Index.js";import i from"./Files/By.js";import o from"./Files/In.js";import r from"./Files/Not.js";import P from"./Files/Pipe.js";class l{Pipe=async(t=e.Pipe)=>await P(this.Plan,t);Not=async t=>(this.Plan.Results=await r(t,this.Plan.Results),this);By=async(t="**/*")=>(this.Plan.Results=await i(t,this.Plan.Paths,this.Plan.Results),this);In=async(t="./")=>{const s=await o(t,this.Plan.Paths);if(s instanceof Map)for(const[a,n]of s)this.Plan.Paths.set(a,n);return this};Plan={Files:0,Debug:2,Info:{},Paths:new Map,Results:new Map,On:{Buffer:"",After:0,Before:0,Input:"",Output:""}};constructor(t=2){this.Plan.Debug=t}}export{l as default};
+import e from "../Option/Index.js";
+import i from "./Files/By.js";
+import o from "./Files/In.js";
+import r from "./Files/Not.js";
+import P from "./Files/Pipe.js";
+class l{Pipe=async(t=e.Pipe)=>await P(this.Plan,t);Not=async t=>(this.Plan.Results=await r(t,this.Plan.Results),this);By=async(t="**/*")=>(this.Plan.Results=await i(t,this.Plan.Paths,this.Plan.Results),this);In=async(t="./")=>{const s=await o(t,this.Plan.Paths);if(s instanceof Map)for(const[a,n]of s)this.Plan.Paths.set(a,n);return this};Plan={Files:0,Debug:2,Info:{},Paths:new Map,Results:new Map,On:{Buffer:"",After:0,Before:0,Input:"",Output:""}};constructor(t=2){this.Plan.Debug=t}}export { l as default };
+
--- Target/Library/Merge.js
+++ Target/Library/Merge.js
@@ -1,1 +1,3 @@
-import{deepmergeCustom as e}from"deepmerge-ts";var m=e({mergeArrays:!1});export{m as default};
+import { deepmergeCustom as e } from "deepmerge-ts";
+var m=e({mergeArrays:!1});export { m as default };
+
