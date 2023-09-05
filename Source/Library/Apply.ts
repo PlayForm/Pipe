df07f438ac6f1caae57cdc5c37f49889e6126838
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-05 23:43:55 +0300

    squash!

--- Source/Library/Apply.ts
+++ Source/Library/Apply.ts
@@ -1,19 +1,19 @@
 /**
- * The function `Apply` takes a function `Function` and a test value `Test`, and applies `Function` to `Test` based
+ * The function `Apply` takes a function `Fn` and a test value `Test`, and applies `Fn` to `Test` based
  * on its type (Map, Set, Array, or other) and returns the result.
- * @param {unknown} Function - Function is a parameter that represents a function. It can be any type of function.
+ * @param {unknown} Fn - Fn is a parameter that represents a function. It can be any type of function.
  * @param {unknown} Test - The `Test` parameter can be any value that you want to apply the function
- * `Function` to. It can be a single value, an array of values, a set of values, or a map of key-value pairs.
- * @returns The function `Apply` returns the result of applying the function `Function` to the input `Test`.
+ * `Fn` to. It can be a single value, an array of values, a set of values, or a map of key-value pairs.
+ * @returns The function `Apply` returns the result of applying the function `Fn` to the input `Test`.
  * The specific return value depends on the type of `Test`:
  */
-export default async (Function: unknown, Test: unknown) => {
-	if (Function instanceof Function) {
+export default async (Fn: unknown, Test: unknown) => {
+	if (Fn instanceof Function) {
 		if (Test instanceof Map) {
 			const Tests = new Map();
 
 			for (const [Key, Value] of Test) {
-				Tests.set(await Function(Key), await Function(Value));
+				Tests.set(await Fn(Key), await Fn(Value));
 			}
 
 			return Tests;
@@ -23,7 +23,7 @@ export default async (Function: unknown, Test: unknown) => {
 			const Tests = new Set();
 
 			for (const El of Test) {
-				Tests.add(await Function(El));
+				Tests.add(await Fn(El));
 			}
 
 			return Tests;
@@ -33,12 +33,12 @@ export default async (Function: unknown, Test: unknown) => {
 			const Tests = new Array();
 
 			for (const Index of Test) {
-				Tests.push(await Function(Index));
+				Tests.push(await Fn(Index));
 			}
 
 			return Tests;
 		}
 
-		return await Function(Test);
+		return await Fn(Test);
 	}
 };
