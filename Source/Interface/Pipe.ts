e9880472d0d5c96f93ee7dbe8fac14f8b23aac27
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-25 14:59:25 +0300

    squash!

--- Source/Interface/Pipe.ts
+++ Source/Interface/Pipe.ts
@@ -1,10 +1,5 @@
 /**
  * @module Pipe
- *
- * @param {Plan} Plan - The `Plan` parameter is an object that represents the execution plan. It
- * contains information about the tasks to be executed and their corresponding inputs and outputs.
- * @param {Action}  - - `Plan`: The plan object that contains the tasks to be executed.
- * @returns The function `Pipe` returns the modified `Plan` object.
  */
 export default interface Type {
 	(Plan: Plan, Action: Action): Promise<Plan>;
@@ -12,3 +7,4 @@ export default interface Type {
 
 import type Action from "../Interface/Action.js";
 import type Plan from "../Interface/Plan.js";
+
--- Target/Function/Pipe.d.ts
+++ Target/Function/Pipe.d.ts
@@ -4,6 +4,11 @@
  * operations based on the plan, handling various callbacks and error handling along the way.
  *
  * @module Pipe
+ *
+ * @param {import('../Interface/Plan.js')} Plan - The `Plan` parameter is an object that represents the execution plan. It
+ * contains information about the tasks to be executed and their corresponding inputs and outputs.
+ * @param {Action}  - - `Plan`: The plan object that contains the tasks to be executed.
+ * @returns The function `Pipe` returns the modified `Plan` object.
  */
 declare const _default: Type;
 export default _default;
