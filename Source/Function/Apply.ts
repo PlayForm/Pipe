e0eeb2b9c6ef9ce98e2701c19d56d5a3c5743b08
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-05 14:50:26 +0300

    squash!

--- Source/Function/Apply.ts
+++ Source/Function/Apply.ts
@@ -1,17 +1,8 @@
 /**
- * The function `Apply` takes a function `_Function` and a test value `Test`, and applies `_Function` to `Test` based
- * on its type (Map, Set, Array, or other) and returns the result.
- *
  * @module Apply
  *
- * @param {unknown} _Function - _Function is a parameter that represents a function. It can be any type of function.
- *
- * @param {unknown} Test - The `Test` parameter can be any value that you want to apply the function `_Function` to. It can be a single value, an array of values, a set of values, or a map of key-value pairs.
- *
- * @returns The function `Apply` returns the result of applying the function `_Function` to the input `Test`. The specific return value depends on the type of `Test`:
- *
  */
-export const _Function: Type = async (_Function: unknown, Test: unknown) => {
+export const _Function = (async (_Function: unknown, Test: unknown) => {
 	if (_Function instanceof Function) {
 		if (Test instanceof Map) {
 			const Tests = new Map();
@@ -45,7 +36,7 @@ export const _Function: Type = async (_Function: unknown, Test: unknown) => {
 
 		return await _Function(Test);
 	}
-};
+}) satisfies Type as Type;
 
 export default _Function;
 
