004c311201e8a23479a22c91819dd8a619217ccb
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-20 20:39:43 +0300

    squash!

--- Target/Function/Bytes.d.ts
+++ Target/Function/Bytes.d.ts
@@ -5,3 +5,4 @@
 declare const _default: Type;
 export default _default;
 import type Type from "../Interface/Bytes.js";
+export declare const Kilobyte = 1024;
--- Target/Function/Bytes.js
+++ Target/Function/Bytes.js
@@ -1,1 +1,1 @@
-var o=async(...[t,e=2])=>{if(t===0)return"0 Bytes";const r=1024,a=Math.floor(Math.log(t)/Math.log(r));return`${parseFloat((t/r**a).toFixed(e<0?0:e))} ${["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][a]}`};export{o as default};
+var a=async(...[t,e=2])=>{if(t===0)return"0 Bytes";const o=Math.floor(Math.log(t)/Math.log(r));return`${parseFloat((t/r**o).toFixed(e<0?0:e))} ${["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][o]}`};const r=1024;export{r as Kilobyte,a as default};
