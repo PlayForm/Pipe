b4bda101350cb363bebf0aa05b4f7ea13d9152a6
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-24 16:33:56 +0200

    squash!

--- Target/Function/Bytes.js
+++ Target/Function/Bytes.js
@@ -1,1 +1,1 @@
-var a=async(...[t,e=2])=>{if(t===0)return"0 Bytes";const o=Math.floor(Math.log(t)/Math.log(r));return`${parseFloat((t/r**o).toFixed(e<0?0:e))} ${["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][o]}`};const r=1024;export{r as Kilobyte,a as default};
+var a=async(...[e,t=2])=>{if(e===0)return"0 Bytes";const r=Math.floor(Math.log(e)/Math.log(o));return`${Number.parseFloat((e/o**r).toFixed(t<0?0:t))} ${["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][r]}`};const o=1024;export{o as Kilobyte,a as default};
