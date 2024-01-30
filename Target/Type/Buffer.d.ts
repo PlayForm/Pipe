79d199c21f692bff928785b6a64f61eee3088424
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-30 15:08:59 +0200

    squash!

--- Target/Type/Buffer.d.ts
+++ Target/Type/Buffer.d.ts
@@ -8,4 +8,4 @@
  */
 export type Type = string | NodeJS.ArrayBufferView | Iterable<string | NodeJS.ArrayBufferView> | AsyncIterable<string | NodeJS.ArrayBufferView> | Stream;
 export type { Type as default };
-import type { Stream } from "node:stream";
+import type { Stream } from "stream";
