ccebdb78861a631b16897487e9cf955579b1bbac
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-22 20:41:07 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/Interface/Merge.d.ts
+++ Target/Interface/Merge.d.ts
@@ -1,21 +1,21 @@
 /**
  * @module Merge
  *
- * Represents a generic interface for deep merging objects using merge functions defined in DeepMergeMergeFunctionsURIs.
+ * Represents a generic interface for deep merging objects using merge functions defined in DeepMergeFunctionsURIs.
  *
- * @template PMF - A type parameter representing Partial<DeepMergeMergeFunctionsURIs>.
+ * @template PMF - A type parameter representing Partial<DeepMergeFunctionsURIs>.
  *
  */
-export default interface Interface<PMF extends Partial<DeepMergeMergeFunctionsURIs>> {
+export default interface Interface<PMF extends Partial<DeepMergeFunctionsURIs>> {
     /**
      * Merges multiple objects of type Ts using the provided merge functions and built-in metadata.
      *
      * @param ...Objects - An arbitrary number of objects to be merged.
      *
      */
-    <Ts extends readonly unknown[]>(...Objects: Ts): DeepMergeHKT<Ts, GetDeepMergeMergeFunctionsURIs<PMF>, DeepMergeBuiltInMetaData>;
+    <Ts extends readonly unknown[]>(...Objects: Ts): DeepMergeHKT<Ts, GetDeepMergeFunctionsURIs<PMF>, DeepMergeBuiltInMetaData>;
 }
 export interface Generic {
     DeepMergeArraysURI: DeepMergeLeafURI;
 }
-import type { DeepMergeBuiltInMetaData, DeepMergeHKT, DeepMergeLeafURI, DeepMergeMergeFunctionsURIs, GetDeepMergeMergeFunctionsURIs } from "deepmerge-ts";
+import type { DeepMergeBuiltInMetaData, DeepMergeHKT, DeepMergeLeafURI, DeepMergeFunctionsURIs, GetDeepMergeFunctionsURIs } from "deepmerge-ts";
